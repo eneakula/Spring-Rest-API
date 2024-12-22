5c8692e431e2172ef187e5ce75ff3701337e1f56
Enea Kula <kulaenea94@gmail.com>
2024-12-22 10:58:22 +0100

    Added Second commitX

--- src/main/java/com/javaTirane42/sda_spring_example_rest_api/config/SecurityConfiguration.java
+++ src/main/java/com/javaTirane42/sda_spring_example_rest_api/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
     @Configuration
@@ -14,7 +15,18 @@ import org.springframework.security.web.SecurityFilterChain;
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
             http.csrf(AbstractHttpConfigurer::disable);
+
+            http.authorizeHttpRequests((requests) -> requests
+                            .requestMatchers("/", "/items/**")
+                            .permitAll()
+                            .anyRequest()
+                            .authenticated()
+                    )
+                    .formLogin((form) -> form
+                            .loginPage("/login")
+                            .permitAll()
+                    )
+                    .logout(LogoutConfigurer::permitAll);
             return http.build();
         }
     }
-
